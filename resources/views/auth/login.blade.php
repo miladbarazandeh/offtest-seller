e184a1e83655874071f1e109a6e4ff481b843ee3
miladbarazandeh <miladbrznd@gmail.com>
2020-07-22 17:08:58 +0430

    auth changed to phone / home page added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,5 @@
 @extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'login', 'title' => __('login')])
 
-{{--@section('content')--}}
-{{--<div class="container">--}}
-    {{--<div class="row justify-content-center">--}}
-        {{--<div class="col-md-8">--}}
-            {{--<div class="card">--}}
-                {{--<div class="card-header">{{ __('Login') }}</div>--}}
-
-                {{--<div class="card-body">--}}
-                    {{--<form method="POST" action="{{ route('login') }}">--}}
-                        {{--@csrf--}}
-
-                        {{--<div class="form-group row">--}}
-                            {{--<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
-
-                                {{--@error('email')--}}
-                                    {{--<span class="invalid-feedback" role="alert">--}}
-                                        {{--<strong>{{ $message }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@enderror--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group row">--}}
-                            {{--<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
-
-                            {{--<div class="col-md-6">--}}
-                                {{--<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">--}}
-
-                                {{--@error('password')--}}
-                                    {{--<span class="invalid-feedback" role="alert">--}}
-                                        {{--<strong>{{ $message }}</strong>--}}
-                                    {{--</span>--}}
-                                {{--@enderror--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group row">--}}
-                            {{--<div class="col-md-6 offset-md-4">--}}
-                                {{--<div class="form-check">--}}
-                                    {{--<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-                                    {{--<label class="form-check-label" for="remember">--}}
-                                        {{--{{ __('Remember Me') }}--}}
-                                    {{--</label>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<div class="form-group row mb-0">--}}
-                            {{--<div class="col-md-8 offset-md-4">--}}
-                                {{--<button type="submit" class="btn btn-primary">--}}
-                                    {{--{{ __('Login') }}--}}
-                                {{--</button>--}}
-
-                                {{--@if (Route::has('password.request'))--}}
-                                    {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-                                        {{--{{ __('Forgot Your Password?') }}--}}
-                                    {{--</a>--}}
-                                {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</form>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-{{--@endsection--}}
-
 
 @section('content')
     <div class="container" style="height: auto;">
@@ -82,24 +10,26 @@
 
                     <div class="card card-login card-hidden mb-3">
                         <div class="card-header card-header-primary text-center">
-                            <h4>ورود</h4>
+                            <h4>{{__('Login')}}</h4>
                         </div>
                         <div class="card-body" style="direction: ltr">
-                            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <div class="bmd-form-group{{ $errors->has('phone') ? ' has-danger' : '' }} mt-3">
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                   <span class="input-group-text">
-                    <i class="material-icons">email</i>
+                    <i class="material-icons">phone_android</i>
                   </span>
                                     </div>
-                                    <input type="email" name="email" class="form-control" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required>
+                                    <input type="tel" name="phone" class="form-control" placeholder="{{ __('Phone Number') }}" value="{{ old('phone') }}" pattern="09[0-9]{9}"required>
                                 </div>
-                                @if ($errors->has('email'))
-                                    <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                                        <strong style="direction: rtl">{{ $errors->first('email') }}</strong>
+                                @if ($errors->has('phone'))
+                                    <div id="email-error" class="error text-danger pl-3" for="phone" style="display: block;">
+                                        <strong>{{ $errors->first('phone') }}</strong>
                                     </div>
                                 @endif
                             </div>
+
+
                             <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
                                 <div class="input-group">
                                     <div class="input-group-prepend">
@@ -107,17 +37,20 @@
                     <i class="material-icons">lock_outline</i>
                   </span>
                                     </div>
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password') }}" value="" required>
+                                    <input type="password" name="password" id="password" class="form-control"
+                                           placeholder="{{ __('Password') }}" value="" required>
                                 </div>
                                 @if ($errors->has('password'))
-                                    <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
+                                    <div id="password-error" class="error text-danger pl-3" for="password"
+                                         style="display: block;">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </div>
                                 @endif
                             </div>
                             <div class="form-check mr-auto ml-3 mt-3">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" style="direction: rtl" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                    <input class="form-check-input" style="direction: rtl" type="checkbox"
+                                           name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
                                     <span class="form-check-sign">
                   <span class="check"></span>
                 </span>
---
+++ resources/views/panel/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app', ['activePage' => 'home', 'titlePage' => __('Home')])
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
