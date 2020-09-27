2a037d1c8f24c0b272b838c78c9ff49f2c00da98
miladbarazandeh <miladbrznd@gmail.com>
2020-09-27 19:04:53 +0330

    seller promotions

---
+++ database/migrations/2020_07_22_222351_create_seller_promotions_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use App\Database\Migrations\BaseMigration;
+use App\Models\SellerPromotion;
+
+class CreateSellerPromotionsTable extends BaseMigration
+{
+    public function __construct()
+    {
+        parent::__construct(SellerPromotion::TABLE);
+    }
+
+    protected function createTable(Blueprint $table)
+    {
+        $table->integer(SellerPromotion::COLUMN_SELLER_ID, false, true)
+            ->nullable(false);
+        $table->enum('status', \App\Seller::STATUS_ALL);
+        $table->string(SellerPromotion::COLUMN_TITLE, 100)
+            ->nullable(false);
+        $table->string(SellerPromotion::COLUMN_PRODUCT_URL, 1000)
+            ->nullable(false);
+        $table->string(SellerPromotion::COLUMN_IMAGE, 100)
+            ->nullable(true);
+        $table->bigInteger(SellerPromotion::COLUMN_FULL_PRICE, false, true)
+            ->nullable(false);
+        $table->bigInteger(SellerPromotion::COLUMN_TESTER_PRICE, false, true)
+            ->nullable(false);
+        $table->integer(SellerPromotion::COLUMN_AVAILABLE_PRODUCTS_COUNT, false, true)
+            ->nullable(false);
+        $table->integer(SellerPromotion::COLUMN_MIN_USER_EXPERIENCE, false, true)
+            ->default(0)
+            ->nullable(false);
+        $table->dateTime(SellerPromotion::COLUMN_START_AT)
+            ->nullable(false);
+        $table->dateTime(SellerPromotion::COLUMN_END_AT)
+            ->nullable(false);
+
+        $sellerIndex = 'index_'.SellerPromotion::TABLE.'_'.SellerPromotion::COLUMN_SELLER_ID;
+        $table->index([SellerPromotion::COLUMN_SELLER_ID], $sellerIndex);
+    }
+
+    protected function alterTable(Blueprint $table)
+    {
+    }
+}
